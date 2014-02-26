1f6d2992fd12c87ce545233ff5c3182e915fe4e2
Sam Sherar <s.sherar@live.com>
2014-02-26 11:50:06 +0000

    adding bootstrap.sh to set the dotfiles up and download dependancies which aren't submodules

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Download oh-my-zsh if not exists
+if [-d ~/.oh-my-zsh]
+then
+  git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+#Copy my style into the template
+cp sambuca.zsh-theme ~/.oh-my.zsh/themes
+
+#Set up the links
+ln -s .zshrc ~/.zshrc
+ln -s .vimrc ~/.vimrc
+ln -s .vim ~/.vim
+ln -s .tmux.conf ~/.tmux.conf
